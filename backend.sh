e219086fb09bdaa48621b53a5b6615cc7ff712b7
sai <saichandrikasasi@gmail.com>
2025-02-21 12:34:20 +0530

    mysql

--- backend.sh
+++ backend.sh
@@ -38,14 +38,14 @@ echo " script started executing at : $(date)"  | tee -a $LOG_FILE
 CHECK_ROOT
 
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>LOG_FILE
 VALIDATE $? "disabling nodejs"
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>>LOG_FILE
 VALIDATE $? "enabling nodejs:20"
 
-dnf install nodejs -y
+dnf install nodejs -y &>>LOG_FILE
 VALIDATE $? "installing nodejs"
 
-useradd expense
+useradd expense &>>LOG_FILE
 VALIDATE $? "creating user expense"
