c549f324d918276560ccb53c5458c64e9c6649ab
sai <saichandrikasasi@gmail.com>
2025-02-21 19:11:04 +0530

    mysql

--- findinglog.sh
+++ findinglog.sh
@@ -1,7 +1,7 @@
 Source_Direct=/home/ec2-user/log
 if [ -d $Source_Direct ]
 then
- echo "Source_Direct exists"
+ echo "$Source_Direct exists"
 else
- echo "Source_Direct exists"
+ echo "$Source_Direct not exists"
 fi 
