e228fb9529e793b5227809acd5a13c29479205ee
sai <saichandrikasasi@gmail.com>
2025-02-21 12:31:22 +0530

    mysql

---
+++ backend.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shell_script"
+SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log
+
+mkdir -p $LOGS_FOLDER
+Userid=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+CHECK_ROOT(){
+
+     if [ $Userid -ne 0 ]
+     then
+       echo -e " $R Please run the script with root previlages" | tee -a $LOG_FILE
+       exit 1  
+     fi
+    }
+
+
+
+VALIDATE(){
+ if [ $1 -ne 0 ]
+ then
+   echo -e "$2 $R is failed" | tee -a $LOG_FILE
+ else 
+   echo -e "$2 $G is success" | tee -a $LOG_FILE
+ fi
+}
+
+
+echo " script started executing at : $(date)"  | tee -a $LOG_FILE
+
+CHECK_ROOT
+
+
+dnf module disable nodejs -y
+VALIDATE $? "disabling nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "enabling nodejs:20"
+
+dnf install nodejs -y
+VALIDATE $? "installing nodejs"
+
+useradd expense
+VALIDATE $? "installing nodejs"
