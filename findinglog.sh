d7d17cda1dce547c836d3a5afc1c10ac491a8358
sai <saichandrikasasi@gmail.com>
2025-02-21 19:15:37 +0530

    mysql

--- findinglog.sh
+++ findinglog.sh
@@ -1,7 +1,13 @@
 Source_Direct=/home/ec2-user/log
+
 if [ -d $Source_Direct ]
 then
- echo "$Source_Direct exists"
+ echo -e "$Source_Direct $G exists$N"
 else
- echo "$Source_Direct not exists"
+ echo -e "$Source_Direct $R not exists$N"
 fi 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+FILES=$(find /home/ec2-user/log/ -name "*.log" -mtime +14)
