0d4ee4f0628793421d6594138f280390f1fe65ae
sai <saichandrikasasi@gmail.com>
2025-02-21 11:37:41 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -5,7 +5,7 @@ SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log
 
-mkdir -p $LOGS_FOLDER
+mkdir -p $LOG_FILE
 Userid=$(id -u)
 
 R="\e[31m"
@@ -16,7 +16,7 @@ CHECK_ROOT(){
 
      if [ $Userid -ne 0 ]
      then
-       echo -e " $R Please run the script with root previlages" &>>$LOG_FILE
+       echo -e " $R Please run the script with root previlages" | tee -a $LOG_FILE
        exit 1  
      fi
     }
@@ -26,22 +26,23 @@ CHECK_ROOT(){
 VALIDATE(){
  if [ $1 -ne 0 ]
  then
-   echo -e "$2 $R is failed" &>>$LOG_FILE
+   echo -e "$2 $R is failed" | tee -a $LOG_FILE
  else 
-   echo -e "$2 $G is success" &>>$LOG_FILE
+   echo -e "$2 $G is success" | tee -a $LOG_FILE
  fi
 }
 CHECK_ROOT
 
-for package in $@
-do
- dnf list installed $package &>>$LOG_FILE
- if [ $? -ne 0 ]
- then 
-  echo -e "$R $package is not installed.. $G going to install" &>>$LOG_FILE
-  dnf install $package -y 
-  VALIDATE $? "Installing $package"
- else
-  echo -e "$G $package is already installed $Y nothing to do" &>>$LOG_FILE
- fi  
-done
+echo " script started executing at : $(date)"  | tee -a $LOG_FILE
+
+dnf install mysql-server -y
+VALIDATE $? "Installing mysql"
+
+systemctl enable mysqld
+VALIDATE $? "enabling mysql"
+
+systemctl start mysqld
+VALIDATE $? "Starting mysql"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "setting root password for expense user"
