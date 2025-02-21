f115cb912297df91af9e7f120da260f5539ff11d
sai <saichandrikasasi@gmail.com>
2025-02-21 11:41:09 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -5,7 +5,7 @@ SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log
 
-mkdir -p $LOG_FILE
+mkdir -p $LOGS_FOLDER
 Userid=$(id -u)
 
 R="\e[31m"
