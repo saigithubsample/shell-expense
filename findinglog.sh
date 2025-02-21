c468491f34de592278207b57d1c314cbbf74c361
sai <saichandrikasasi@gmail.com>
2025-02-21 19:17:57 +0530

    mysql

--- findinglog.sh
+++ findinglog.sh
@@ -1,3 +1,8 @@
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 Source_Direct=/home/ec2-user/log
 
 if [ -d $Source_Direct ]
@@ -6,10 +11,7 @@ then
 else
  echo -e "$Source_Direct $R not exists$N"
 fi 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+
 FILES=$(find $Source_Direct -name "*.log" -mtime +14)
 
  echo "list of files: $FILES"
