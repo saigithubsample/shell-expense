f6de3c0b56f47afc56c1a6b26b131e5c21e0f177
sai <saichandrikasasi@gmail.com>
2025-02-22 12:21:03 +0530

    logs

--- findinglog.sh
+++ findinglog.sh
@@ -16,7 +16,7 @@ FILES=$(find $Source_Direct -name "*.log" -mtime +14)
 
  echo "list of files: $FILES"
 
- while IFS=read -r line
+ while IFS= read -r line
  do
    echo "Deleting line: $line"
    rm -rf $line
